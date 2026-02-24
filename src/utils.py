42ba396f5c160be0bf0a3c0ad4c74e3731ada495
meetv <meet.vasani2810@gmail.com>
2026-02-24 13:28:53 +0530

    Logger utils

---
+++ src/utils.py
@@ -0,0 +1,15 @@
+import logging
+
+def setup_logger(name: str = "dataprep"):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        ch = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(message)s"
+        )
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+
+    return logger
